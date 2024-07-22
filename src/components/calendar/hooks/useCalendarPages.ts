7d7b9525fa2f39a5cdad9e852c9c0ab214702006
son-ngo <ngotruongsonha@gmail.com>
2024-07-22 09:01:10 +0200

    save

---
+++ src/components/calendar/hooks/useCalendarPages.ts
@@ -0,0 +1,92 @@
+import dayjs from "dayjs";
+import { useCallback, useEffect, useRef, useState, useMemo } from 'react';
+import * as React from "react";
+
+interface UseCalendarPagesProps {
+    minDate?: Date | string | dayjs.Dayjs,
+    maxDate?: Date | string | dayjs.Dayjs,
+
+    typePage: 'month' | 'period' | 'week',
+    numberOfDays?: number, //only apply for period
+    referenceOfPeriod?: Date | string | dayjs.Dayjs, //reference for every start of period, not including for month
+}
+
+const UseCalendarPages = ({
+      minDate,
+      maxDate,
+      typePage,
+      numberOfDays = 7,
+      referenceOfPeriod
+}: UseCalendarPagesProps) => {
+    const _referenceOfPeriod = useMemo(() => dayjs(referenceOfPeriod), [referenceOfPeriod]);
+
+    const getPeriod = React.useCallback ((date?: dayjs.Dayjs | string | Date) => {
+        //TODO: test this function
+        if (typePage === 'month') {
+            return dayjs(date).startOf('month');
+        } else if (typePage === 'week'  || ( typePage === 'period' && numberOfDays === 7 && !referenceOfPeriod )) {
+            return dayjs(date).startOf('isoWeek');
+        }
+
+        let deltaPeriod = Math.floor( dayjs(date).diff(_referenceOfPeriod, 'days', true) / numberOfDays );
+        return _referenceOfPeriod.add( deltaPeriod * numberOfDays, 'days');
+    }, [referenceOfPeriod, numberOfDays]);
+
+    const _minDate = useMemo(() => getPeriod(minDate), [minDate]);
+    const _maxDate = useMemo(() => getPeriod(maxDate), [maxDate]);
+
+    const _generateListDate = useCallback(() => {
+        let pages : dayjs.Dayjs[] = [];
+        let thisDate = _minDate;
+
+        while (thisDate.isBefore(_maxDate)) {
+            pages.push( thisDate );
+            thisDate =  typePage === 'month'
+                ? thisDate.add(1, 'month')
+                :   typePage === 'week'
+                    ? thisDate.add(1, 'week')
+                    : thisDate.add(numberOfDays, 'days');
+        }
+
+        return pages;
+    }, [typePage, numberOfDays, _minDate, _maxDate]);
+
+
+    const pagesRef = useRef(_generateListDate());
+    const [ pagesLength, setPagesLength ] = useState( pagesRef.current.length );
+
+    useEffect(() => {
+        pagesRef.current = _generateListDate();
+        setPagesLength(pagesRef.current.length);
+    }, [_minDate, _maxDate, typePage, numberOfDays]);
+
+    const getIndexOfPage = useCallback((date?: dayjs.Dayjs | string | Date) => {
+        if (typePage === 'month') {
+            return Math.floor(dayjs(date).diff(_minDate, 'months'));
+        }
+        if (typePage === 'week' || ( typePage === 'period' && numberOfDays === 7 && !referenceOfPeriod )) {
+            return Math.floor(dayjs(date).diff(_minDate, 'weeks'));
+        }
+
+        return Math.floor(dayjs(date).diff(_minDate, 'days', true) / numberOfDays);
+    }, []);
+
+    const isOutOfRange = useCallback((date: dayjs.Dayjs | string | Date) : 'left' | 'right' | 'none' => {
+        return getPeriod(date).isBefore(_minDate) ? 'left' : getPeriod(date).isAfter(_maxDate) ? 'right' : 'none';
+    }, []);
+
+    const isOnEdgePages = useCallback((date: dayjs.Dayjs | string | Date) : 'left' | 'right' | 'none' => {
+        return getIndexOfPage(date) === 0 ? 'left' : getIndexOfPage(date) === pagesLength - 1 ? 'right' : 'none';
+    }, []);
+
+    return {
+        getPeriod,
+        pagesRef,
+        pagesLength,
+        isOutOfRange,
+        isOnEdgePages,
+        getIndexOfPage,
+    };
+};
+
+export default UseCalendarPages;
---
+++ src/components/calendar/timeline/TimelineTimebar.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { useTheme } from '@react-navigation/native';
+import { View, StyleSheet, Text } from 'react-native';
+
+//constants
+import {
+    TIMELINE_TIME_BAR_WIDTH,
+    START_HOUR, END_HOUR, TIMELINE_CELL_HEIGHT, CALENDAR_BODY_HEIGHT
+} from '../constants';
+
+//components
+import {Outlines, Typography} from '../../../styles';
+import {type TimelineProps} from "./Timeline";
+import {SyncedScrollView} from "../../atomic";
+
+type TimelineTimebarProps = {
+    height?: TimelineProps['height']
+    headerHeight?: number,
+}
+
+const TimelineTimebar : React.FC<TimelineTimebarProps> = ({
+    height = CALENDAR_BODY_HEIGHT,
+    headerHeight = 20,
+}) => {
+
+    const { colors } = useTheme();
+
+    const renderTimeBar = React.useCallback<() => React.ReactNode>(() => {
+        const cells : React.ReactNode[] = [];
+        for (let i = START_HOUR + 1; i <= END_HOUR; i++) {
+            cells.push(
+                <Text   key={i}
+                        style={[ styles.timeBarCell,
+                                {...Typography.body.x10, color: colors.border, fontSize: 11}]}
+                >{`${ i<10 ? '0'+i : i }:00`}</Text>
+            )
+        }
+        return (
+            <View style={{marginTop: TIMELINE_CELL_HEIGHT / 2 + 9}}
+                    onStartShouldSetResponder={() => true}>
+                {cells.map( cell => cell )}
+            </View>
+        )
+    }, [colors.border]);
+
+    return (
+        <View style={[styles.timeBarContainer, {borderColor: colors.border}]}>
+            <View style={[{height: headerHeight, borderColor: colors.border}, styles.timeBarHeader]} />
+            <SyncedScrollView
+                _id={0}
+                style={[{maxHeight: height } ]}
+                showsVerticalScrollIndicator={false}
+            >
+                {renderTimeBar()}
+            </SyncedScrollView>
+        </View>
+    )
+};
+
+export default React.memo(TimelineTimebar);
+
+const styles = StyleSheet.create({
+    timeBarContainer: {
+        width: TIMELINE_TIME_BAR_WIDTH,
+        borderRightWidth: Outlines.borderWidth.thin,
+    },
+    timeBarCell : {
+        height: TIMELINE_CELL_HEIGHT,
+        backgroundColor: 'transparent',
+        marginRight: 7,
+    },
+    timeBarHeader: {
+        borderBottomWidth: Outlines.borderWidth.thin,
+    }
+});
